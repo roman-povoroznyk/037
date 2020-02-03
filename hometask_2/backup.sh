c2a2ff3d99b5f322eafc5ea37b87695c4884c851
Roman Povoroznyk <34652253+roman-povoroznyk@users.noreply.github.com>
2020-02-03 14:34:43 +0200

    fixed backup function

--- hometask_2/backup.sh
+++ hometask_2/backup.sh
@@ -10,7 +10,7 @@ then
 			if [[ $(mkdir -p $2/$(date +%Y%m%d)) -eq 0 ]]
 			then
 				echo "// Copying file(s)."
-				if [[ $(find $1 -ctime -1 -exec cp -rf --parents {} $2/$(date +%Y%m%d) \;) -eq 0 ]]
+				if [[ $(find $1 -ctime -1 -exec rsync -R {} $2/$(date +%Y%m%d) \;) ]]
 				then
 					echo "// Copied file(s) to $2/$(date +%Y%m%d)"
 				else
@@ -23,7 +23,7 @@ then
 		elif [[ -d $2 ]]
 		then
 			$(mkdir -p $2/$(date +%Y%m%d))
-			if [[ $(find $1 -ctime -1 -exec cp -rf --parents {} $2/$(date +%Y%m%d) \;) -eq 0 ]]
+			if [[ $(find $1 -ctime -1 -exec rsync -R {} $2/$(date +%Y%m%d) \;) ]]
                 	then
                         	echo "// Copied file(s) to $2/$(date +%Y%m%d)"
                 	else
