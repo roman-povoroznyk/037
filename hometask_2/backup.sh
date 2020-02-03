dbd97d47ce6b86ea918a7beb075281893046e308
Roman Povoroznyk <34652253+roman-povoroznyk@users.noreply.github.com>
2020-02-03 14:51:21 +0200

    Update backup.sh

--- hometask_2/backup.sh
+++ hometask_2/backup.sh
@@ -6,15 +6,16 @@ then
 		echo "// New or modificated file(s) found."
 		if [[ -n $2 && ! -d $2 ]]
 		then
-			echo "// Creating backup folder $2."
+			echo "// Creating backup folder \"$2\"."
 			if [[ $(mkdir -p $2/$(date +%Y%m%d)) -eq 0 ]]
 			then
 				echo "// Copying file(s)."
 				if [[ $(find $1 -ctime -1 -exec rsync -R {} $2/$(date +%Y%m%d) \;) ]]
 				then
-					echo "// Copied file(s) to $2/$(date +%Y%m%d)"
+					echo "// Copied file(s) to \"$2/$(date +%Y%m%d)/\"."
 				else
 					echo "// Can't copy file(s)."
+					exit
 				fi
 			else 
 				echo "// Can't create backup folder."
@@ -25,15 +26,18 @@ then
 			$(mkdir -p $2/$(date +%Y%m%d))
 			if [[ $(find $1 -ctime -1 -exec rsync -R {} $2/$(date +%Y%m%d) \;) ]]
                 	then
-                        	echo "// Copied file(s) to $2/$(date +%Y%m%d)"
+                        	echo "// Copied file(s) to \"$2/$(date +%Y%m%d)\"."
                 	else
                                 echo "// Can't copy file(s)."
+				exit
                         fi
 		else
-			echo "// Backup directory error."	
+			echo "// Backup directory error."
+			exit
 		fi
 	else
 		echo "// No file(s) found."
+		exit
 	fi
 else
 	echo "// Source directory error."
