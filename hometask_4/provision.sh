4028acc25be0358181f2406465ac7feb5e710f11
Roman Povoroznyk <34652253+roman-povoroznyk@users.noreply.github.com>
2020-02-18 12:49:19 +0200

    Update provision.sh

--- hometask_4/provision.sh
+++ hometask_4/provision.sh
@@ -16,13 +16,12 @@ add-apt-repository \
 apt update
 apt install -y docker-ce
 
-docker run -d -p 80:80 --name mysql romashultz/mysql
-docker run -d --name apache-wordpress romashultz/apache-wordpress
+docker run -d --name mysql romashultz/mysql
+docker run -d -p 80:80 --name apache-wordpress romashultz/apache-wordpress
 docker network create wp-msql
 docker network connect wp-msql mysql
 docker network connect wp-msql apache-wordpress
 docker exec mysql mysql -u root -p --skip-password -e "CREATE DATABASE wordpress;"
 docker exec mysql mysql -u root -p --skip-password -e "CREATE USER 'wordpress'@'172.18.0.3' IDENTIFIED BY 'wordpress';"
 docker exec mysql mysql -u root -p --skip-password -e "GRANT ALL ON wordpress.* TO 'wordpress'@'172.18.0.3';"
-docker exec apache-wordpress chown -R www-data:www-data /var/www/html
 echo -e "Username: wordpress\nPassword: wordpress\nDatabase: wordpress\nLocalhost: 172.18.0.3" 
