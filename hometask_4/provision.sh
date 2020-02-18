4812aa51deff0bd5064a2b8d3f05ee5ab78faf84
Roman Povoroznyk <34652253+roman-povoroznyk@users.noreply.github.com>
2020-02-18 12:10:58 +0200

    Add files via upload

---
+++ hometask_4/provision.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+MAIN_FOLDER="/vagrant"
+SERVER_FILE=$MAIN_FOLDER/"atlassian-confluence-7.3.1-x64.bin"
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt update
+apt install -y docker-ce
+
+docker run -d -p 80:80 --name mysql romashultz/mysql
+docker run -d -p --name apache-wordpress romashultz/apache-wordpress
+docker network create wp-msql
+docker network connect wp-msql mysql
+docker network connect wp-msql apache-wordpress
+docker exec mysql mysql -u root -p --skip-password -e "CREATE DATABASE wordpress;"
+docker exec mysql mysql -u root -p --skip-password -e "CREATE USER 'wordpress'@'172.18.0.3' IDENTIFIED BY 'wordpress';"
+docker exec mysql mysql -u root -p --skip-password -e "GRANT ALL ON wordpress.* TO 'wordpress'@'172.18.0.3';"
+
+echo -e "Username: wordpress\nPassword: wordpress\nDatabase: wordpress\nLocalhost: 172.18.0.3" 
+
+#docker exec wordpress chown -R www-data:www-data /var/www/html # So Apache is able to manage the WordPress files
+
+
+
+
